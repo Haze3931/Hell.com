c34a823e17c3b0c039ce6b2803c595add27742c8
Haze3931 <62397736+Haze3931@users.noreply.github.com>
2021-02-11 17:50:39 +0100

    .net

---
+++ JigokuShōjo.Net/Revenge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace JigokuShōjo.Net
+{
+    public class Revenge
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+    }
+}
