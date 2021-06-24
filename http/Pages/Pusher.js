1186cf3d28dfc36b42a46c59f0bb153753d9687d
DeVianney <jacquesvianney3931@gmail.com>
2021-06-24 16:24:51 +0200

    Delete http directory

--- http/Pages/Pusher.js
+++
@@ -1,6 +0,0 @@
-﻿function Pusher() {
-    var gif = document.getElementById("opener");
-    gif.style.display = "none";
-    document.getElementsByClassName("MainDiv")[0].style.visibility = "visible";
-
-}
