dd54f922e3287817cf48c7996bf4e38a59f359b2
DeVianney <jacquesvianney3931@gmail.com>
2021-06-25 18:12:13 +0200

    1poprawkadokonstytucji

--- server.cc
+++ server.cc
@@ -14,6 +14,13 @@
 #define PORT 80
 #define HOST "127.0.0.1"
 
+void erro(const char *str,UINT exitcode);
+void erro(const char *str);
+void init(std::vector<char> buff,char (&verb)[200],char (&pth)[200]);
+void parse(char (&mtype)[50],char (&type)[20],char (&pth)[200]);
+std::vector<char> loadfile(char (&pth)[200]);
+void sendata(SOCKET n_socket,char (&status_code)[4],char (&mtype)[50],std::vector<char> dataa);
+int connection(SOCKET n_socket);
 
 //formaty: .html .gif .ico .css .js
 // resp on get i push
@@ -21,125 +28,7 @@
 
 //g++ server.cc -o serwer.exe -lws2_32 -std=c++11 -pthread
 
-
-void erro(const char *str,UINT exitcode){
-	printf("%s\n",str);
-	ExitProcess(exitcode);
-}
-void erro(const char *str){
-	printf("%s\n",str);
-}
-
-
-int connection(SOCKET n_socket){
-
-	char recvbuf[0x1024];
-	int  recvbuflen = 0x1024;
-	bool req = false;
-
-	recv(n_socket,recvbuf,recvbuflen,0);
-
-	int i = 0;
-	std::vector<char> finalbuf;
-	for (i; i < recvbuflen; ++i)
-	{
-		if(recvbuf[i] == '\r' && recvbuf[i + 3] == '\n'){req = true;break;};
-		finalbuf.push_back(recvbuf[i]);
-	}
-	if(!req){
-	   erro("Bad Request");
-	   closesocket(n_socket);
-	   return 3;
-	}
-
- 	char verb[200] = " ";
- 	char pth[200] = " ";
- 	bool flag = true;
-	int itr = 0;
- 	for(auto x: finalbuf){ 
- 		itr++;
- 		if (flag)
- 		{
- 			verb[itr] += x;
- 		}
- 		else{
- 			
- 			pth[itr] += x;
- 			
- 		}
- 		if(x == ' ')
- 		{
- 			if (flag == false)
- 			{
- 				break;
- 			}
- 			flag = false;
- 			itr = 0;
- 			
- 		}
-
- 	}
-
-	char mtype[50] = " ";
-	if(pth[1] == '/' && pth[2] == ' '){
-
-		strcpy(pth,"index.html");
-
-	}
-	else{
-		bool frst = true;
-		int j = 0;
-		for (int i = 0; i < 200; ++i)
-		{
-			j++;
-			if (frst)
-			{
-				i--;
-				frst = false;
-				continue;
-			}
-			if (pth[i] == 0)
-			{
-				break;
-			}
-			pth[i] = pth[j];
-
-		}
-
-	}
-
-	char type[20] = "";
-	int state = 0;
-	int itrto = 0; 
-	for (int i = 0; i < 200; ++i)
-	{
-
-		if (pth[i] == 0)
-		{
-			break;
-		}
-		if(pth[i] == '.'){
-			state = 2;
-
-		}
-		if(state == 2){
-			type[itrto++] += pth[i];
-		}
-
-	}  
-	
-
-    std::ifstream input(pth, std::ios::binary);
-
-    std::vector<char> dataa(
-         (std::istreambuf_iterator<char>(input)),
-         (std::istreambuf_iterator<char>()));
-
-    input.close();
-
-
-	
-	std::map<char*, char*> mime = {
+std::map<char*, char*> mime = {
     { ".ico","image/vnd.microsoft.icon"},
     { ".gif", "image/vnd.sealedmedia.softseal.gif" },
     { ".js", "text/javascript"},
@@ -147,62 +36,6 @@ int connection(SOCKET n_socket){
     { ".html", "text/html;charset=utf-8"}
 				};
 
-for (auto& x: mime) {
-	if (type[1] == x.first[1])
-	{
-		strcpy(mtype,x.second);
-		break;
-	}
-	else{
-		strcpy(mtype,"application/binary");
-	}
-    
-  }
-
-
-char request[300] = " ";
-
-
-	strcpy(request,"HTTP/1.1 ");
-send(n_socket,request, (int)strlen(request), 0);
-
-	strcpy(request,"200 OK");
-send(n_socket,request, (int)strlen(request), 0);
-
-	strcpy(request,"\r\n");
-send(n_socket,request, (int)strlen(request), 0);
-
-	strcpy(request,"Content-Type: ");
-send(n_socket,request, (int)strlen(request), 0);
-
-	strcpy(request,mtype);
-send(n_socket,request, (int)strlen(request), 0);
-
-	strcpy(request,"\r\n");
-send(n_socket,request, (int)strlen(request), 0);
-
-	strcpy(request,"Content-Length: ");
-send(n_socket,request, (int)strlen(request), 0);
-
-
-std::string s = std::to_string(dataa.size());
-char const *pchar = s.c_str(); 
-
-send(n_socket,pchar, (int)strlen(pchar), 0);
-
-	strcpy(request,"\r\n");
-send(n_socket,request, (int)strlen(request), 0);
-
-	strcpy(request,"\r\n");
-send(n_socket,request, (int)strlen(request), 0);
-
-
-	send(n_socket, (const char*)dataa.data(),  dataa.size(), 0);
-
-
-	closesocket(n_socket);
-	return 0;
-}
 
 int main(void)
 {
@@ -270,4 +103,205 @@ int main(void)
 
 
 }
+
+
+void erro(const char *str,UINT exitcode){
+	printf("%s\n",str);
+	ExitProcess(exitcode);
+}
+void erro(const char *str){
+	printf("%s\n",str);
+}
+
+void init(std::vector<char> buff,char (&verb)[200],char (&pth)[200]){
+
+ 	bool flag = true;
+	int itr = 0;
+ 	for(auto x: buff){ 
+ 		itr++;
+ 		if (flag)
+ 		{
+ 			verb[itr] += x;
+ 		}
+ 		else{
+ 			
+ 			pth[itr] += x;
+ 			
+ 		}
+ 		if(x == ' ')
+ 		{
+ 			if (flag == false)
+ 			{
+ 				break;
+ 			}
+ 			flag = false;
+ 			itr = 0;
+ 			
+ 		}
+
+ 	}
+
+}
+
+void parse(char (&mtype)[50],char (&type)[20],char (&pth)[200]){
+	if(pth[1] == '/' && pth[2] == ' '){
+
+		strcpy(pth,"index.html");
+
+	}
+	else{
+		bool frst = true;
+		int j = 0;
+		for (int i = 0; i < 200; ++i)
+		{
+			j++;
+			if (frst)
+			{
+				i--;
+				frst = false;
+				continue;
+			}
+			if (pth[i] == 0)
+			{
+				break;
+			}
+			pth[i] = pth[j];
+
+		}
+
+	}
+
+	int state = 0;
+	int itrto = 0; 
+	for (int i = 0; i < 200; ++i)
+	{
+
+		if (pth[i] == 0)
+		{
+			break;
+		}
+		if(pth[i] == '.'){
+			state = 2;
+
+		}
+		if(state == 2){
+			type[itrto++] += pth[i];
+		}
+
+	}  
+	
+
+}
+
+std::vector<char> loadfile(char (&pth)[200]){
+
+    std::ifstream input(pth, std::ios::binary);
+
+    std::vector<char> data(
+         (std::istreambuf_iterator<char>(input)),
+         (std::istreambuf_iterator<char>()));
+
+    input.close();
+
+    return data;
+
+}
+
+void sendata(SOCKET n_socket,char (&status_code)[4],char (&mtype)[50],std::vector<char> dataa){
+
+	char request[300] = " ";
+
+
+	strcpy(request,"HTTP/1.1 ");
+send(n_socket,request, (int)strlen(request), 0);
+
+	strcpy(request,"200 OK");
+send(n_socket,request, (int)strlen(request), 0);
+
+	strcpy(request,"\r\n");
+send(n_socket,request, (int)strlen(request), 0);
+
+	strcpy(request,"Content-Type: ");
+send(n_socket,request, (int)strlen(request), 0);
+
+	strcpy(request,mtype);
+send(n_socket,request, (int)strlen(request), 0);
+
+	strcpy(request,"\r\n");
+send(n_socket,request, (int)strlen(request), 0);
+
+	strcpy(request,"Content-Length: ");
+send(n_socket,request, (int)strlen(request), 0);
+
+
+std::string s = std::to_string(dataa.size());
+
+send(n_socket,(const char*)s.c_str(), (int)strlen((const char*)s.c_str()), 0);
+
+	strcpy(request,"\r\n");
+send(n_socket,request, (int)strlen(request), 0);
+
+	strcpy(request,"\r\n");
+send(n_socket,request, (int)strlen(request), 0);
+
+
+	send(n_socket, (const char*)dataa.data(),  dataa.size(), 0);
+
+
+	closesocket(n_socket);
+
+}
+
+int connection(SOCKET n_socket){
+
+	char recvbuf[0x1024];
+	int  recvbuflen = 0x1024;
+	bool req = false;
+
+	recv(n_socket,recvbuf,recvbuflen,0);
+
+	int i = 0;
+	std::vector<char> finalbuf;
+	for (i; i < recvbuflen; ++i)
+	{
+		if(recvbuf[i] == '\r' && recvbuf[i + 3] == '\n'){req = true;break;};
+		finalbuf.push_back(recvbuf[i]);
+	}
+	if(!req){
+	   erro("Bad Request");
+	   closesocket(n_socket);
+	   return 3;
+	}
+
+ 	char verb[200] = " ";
+ 	char pth[200] = " ";
+
+ 	init(finalbuf,verb,pth);
+
+	char mtype[50] = " ";
+	char type[20] = "";
+	char status_code[4] = "200";
+	parse(mtype,type,pth);
+	
+	std::vector<char> dataa = loadfile(pth);
+
+	//Check mime type 
+	for (auto& x: mime) {
+		if (type[1] == x.first[1])
+		{
+			strcpy(mtype,x.second);
+			break;
+		}
+		else{
+			strcpy(mtype,"application/binary");
+		}
+	    
+	  }
+
+  	sendata(n_socket,status_code,mtype,dataa);
+
+
+	return 0;
+}
+
 
